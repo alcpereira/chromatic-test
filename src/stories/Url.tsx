ddb788ad4001fd10c839b0863038e31eea49e7b9
Alexandre Pereira <alexandre.carneiro.pereira@gmail.com>
2024-08-07 13:01:52 +0100

    chore: improve detection

--- src/stories/Url.tsx
+++ src/stories/Url.tsx
@@ -1,6 +1,14 @@
 const UrlComponent = () => {
   const currentUrl = window.location.href;
-  return <p>{currentUrl}</p>;
+  const navigator = window.navigator;
+  return (
+    <div>
+      <p>url: {currentUrl}</p>
+      <p>navigator.userAgent: {navigator.userAgent}</p>
+      <p>navigator.webdriver: {JSON.stringify(navigator.webdriver)}</p>
+      <p>navigator.vendor: {navigator.vendor}</p>
+    </div>
+  );
 };
 
 export default UrlComponent;
